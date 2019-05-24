e2eddd9ab3b840baf072e5c1d25576c3b4813419
MkLHX <mickael.lehoux@gmail.com>
2019-05-24 14:19:48 +0200

    update readme and lib dep

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-# example how to use ESP pH read library
->[DFRobot ESP PH BY GREENPONIK](https://github.com/GreenPonik/DFRobot_ESP_PH_BY_GREENPONIK)
+# example how to use ESP pH read with ADC ads1115 library
+>[DFRobot ESP PH WITH ADC BY GREENPONIK](https://github.com/GreenPonik/DFRobot_ESP_PH_WITH_ADC_BY_GREENPONIK)
 
 ## #1 clone the repo
-> git clone https://github.com/GreenPonik/esp32-read-ph-example.git
+> git clone https://github.com/GreenPonik/esp32-read-ph-with-ADC-example.git
 > 
-> cd esp32-read-ph-example
+> cd esp32-read-ph-with-ADC-example
 
 ## #2 run platformio build and upload on esp32
 > pio run -t upload
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include <Arduino.h>
-#include "DFRobot_ESP_PH.h"
+#include "DFRobot_ESP_PH_WITH_ADC.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include "Adafruit_ADS1015.h"
 
-DFRobot_ESP_PH ph;
-#define ESPADC 4096.0   //the esp Analog Digital Convertion value
-#define ESPVOLTAGE 3300 //the esp voltage supply value
-#define PH_PIN 35		//the esp gpio data pin number
+#define ONE_WIRE_BUS 15
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+DFRobot_ESP_PH_WITH_ADC ph;
+Adafruit_ADS1115 ads;
+
 float voltage, phValue, temperature = 25;
+
+float readTemperature()
+{
+	//add your code here to get the temperature from your temperature sensor
+	sensors.requestTemperatures();
+	return sensors.getTempCByIndex(0);
+}
 
 void setup()
 {
 	Serial.begin(115200);
 	ph.begin();
+	sensors.begin();
+	ads.setGain(GAIN_ONE);
+	ads.begin();
 }
 
 void loop()
@@ -19,12 +35,17 @@ void loop()
 	if (millis() - timepoint > 1000U) //time interval: 1s
 	{
 		timepoint = millis();
-		//voltage = rawPinValue / esp32ADC * esp32Vin
-		voltage = analogRead(PH_PIN) / ESPADC * ESPVOLTAGE; // read the voltage
+		/**
+		 * index 0 for adc's pin A0
+ 		 * index 1 for adc's pin A1
+		 * index 2 for adc's pin A2
+		 * index 3 for adc's pin A3
+		*/
+		voltage = ads.readADC_SingleEnded(0) / 10; // read the voltage
 		Serial.print("voltage:");
 		Serial.println(voltage, 4);
-		
-		//temperature = readTemperature();  // read your temperature sensor to execute temperature compensation
+
+		temperature = readTemperature(); // read your temperature sensor to execute temperature compensation
 		Serial.print("temperature:");
 		Serial.print(temperature, 1);
 		Serial.println("^C");
@@ -34,9 +55,4 @@ void loop()
 		Serial.println(phValue, 4);
 	}
 	ph.calibration(voltage, temperature); // calibration process by Serail CMD
-}
-
-float readTemperature()
-{
-	//add your code here to get the temperature from your temperature sensor
 }
